458be6f9d95f066b3afcd0b9885fa869066edfe7
karis courey <kariscourey@gmail.com>
2022-11-18 00:39:10 -0500

    incorporate authentication

--- api/routers/saved_news_items.py
+++ api/routers/saved_news_items.py
@@ -15,7 +15,7 @@ router = APIRouter()
 
 @router.get("/api/saved_news_items", response_model=SavedNewsItemsOut)
 def get_all_saved_news_items(
-    # account_data: dict = Depends(authenticator.get_current_account_data),
+    # user_id: int = Depends(authenticator.get_current_account_data)['account']['id'],
     queries: SavedNewsItemQueries = Depends()
     ):
     return {
@@ -25,7 +25,7 @@ def get_all_saved_news_items(
 
 @router.post("/api/saved_news_items/", response_model=SavedNewsItemOut)
 def create_saved_news_item(
-    # account_data: dict = Depends(authenticator.get_current_account_data),
+    # user_id: int = Depends(authenticator.get_current_account_data)['account']['id'],
     news_item_in: SavedNewsItemIn,
     queries: SavedNewsItemQueries = Depends()
     ):
@@ -34,7 +34,7 @@ def create_saved_news_item(
 
 @router.delete("/api/saved_news_items/{news_item_id}", response_model=bool)
 def delete_saved_news_item(
-    # account_data: dict = Depends(authenticator.get_current_account_data),
+    # user_id: int = Depends(authenticator.get_current_account_data)['account']['id'],
     news_item_id: int,
     queries: SavedNewsItemQueries = Depends()
     ):
