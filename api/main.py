a03f738141956dd481f7b14346193976a19bdc29
karis courey <kariscourey@gmail.com>
2022-11-18 13:43:34 -0500

    update main

--- api/main.py
+++ api/main.py
@@ -15,17 +15,3 @@ app.include_router(portfolio_stocks.router)
 app.include_router(saved_items.router)
 app.include_router(saved_news_items.router)
 app.include_router(saved_stocks.router)
-
-oath2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@app.get("/users/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
-
-
-class User(BaseModel):
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
