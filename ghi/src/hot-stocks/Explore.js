f8d2b4f956299f8add6e1fc266dc601d63764ae4
Micheline <MDoughlin@hotmail.com>
2022-11-21 23:49:21 -0500

    Fixed

---
+++ ghi/src/hot-stocks/Explore.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+export function Explore(props) {
+
+    return (
+
+        <div>
+            exploreexploreexploreexploreexploreexploreexplore
+        </div>
+      );
+
+}
