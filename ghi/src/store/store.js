a061d00503875c701050705ccefb0c40461e34a2
SosjoBosjo <Ben.Sjostrom@gmail.com>
2022-11-23 12:13:53 -0600

    develop redux mutations, develop ACL for get_stock

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -3,18 +3,26 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { portfolioStocksApi } from './portfolioStocksApi';
 import { savedItemsApi } from './savedItemsApi';
 import { accountsApi } from './accountsApi';
+import { newsItemsApi} from './newsItemsApi';
+import { stocksApi } from './stocksApi';
 
 export const store = configureStore({
   reducer: {
     [portfolioStocksApi.reducerPath]: portfolioStocksApi.reducer,
     [savedItemsApi.reducerPath]: savedItemsApi.reducer,
     [accountsApi.reducerPath]: accountsApi.reducer,
+    [newsItemsApi.reducerPath]: newsItemsApi.reducer,
+    [stocksApi.reducerPath]: stocksApi.reducer
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware()
+        .concat(accountsApi.middleware)
         .concat(portfolioStocksApi.middleware)
         .concat(savedItemsApi.middleware)
-        .concat(accountsApi.middleware),
+        .concat(stocksApi.middleware)
+        .concat(newsItemsApi.middleware)
+
+
 });
 
 setupListeners(store.dispatch);
