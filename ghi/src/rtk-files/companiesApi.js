ab2153afed2f9013c7ef7f9dca5c79812ade5e9d
karis courey <kariscourey@gmail.com>
2022-12-01 11:50:05 -0500

    update companiesApi with headers

--- ghi/src/rtk-files/companiesApi.js
+++ ghi/src/rtk-files/companiesApi.js
@@ -1,10 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { authApi } from './authApi';
 
 export const companiesApi = createApi({
     reducerPath: 'companies',
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.API_SERVICE,
-    }),
+        baseUrl: 'http://localhost:8000/', //process.env.API_SERVICE,
+        prepareHeaders: (headers, { getState }) => {
+          const selector = authApi.endpoints.getToken.select();
+          const { data: tokenData } = selector(getState());
+          if (tokenData && tokenData.access_token) {
+            headers.set('Authorization', `Bearer ${tokenData.access_token}`);
+          }
+          return headers;
+        }
+      }),
     endpoints: builder => ({
         getCompany: builder.query({
             query: (symbol) => `/api/companies/${symbol}/`,
