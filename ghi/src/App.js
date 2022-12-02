283c67cffea4620a143fa7b15048b4e1f1fec63e
SosjoBosjo <Ben.Sjostrom@gmail.com>
2022-12-02 16:03:41 -0600

    functional chart component testing

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -7,7 +7,7 @@ import { Explore } from './hot-stocks/Explore';
 import { Portfolio } from './hot-stocks/Portfolio';
 import { Saved } from './hot-stocks/Saved';
 import { SearchList } from './hot-stocks/SearchList';
-import QuoteAndChart from './portfolio/QuoteAndChart';
+import QuoteAndChartClass from './portfolio/QuoteAndChartClass';
 
 function App() {
   return (
