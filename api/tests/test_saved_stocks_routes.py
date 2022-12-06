41c65fc083512f73787f2f3a988d1f96741b0a75
yorkpshaw <york.p.shaw@gmail.com>
2022-12-06 17:44:36 -0600

    push for git in group

--- api/tests/test_saved_stocks_routes.py
+++ api/tests/test_saved_stocks_routes.py
@@ -1,42 +1,42 @@
-# from main import app
-# from queries.saved_stocks import SavedStockQueries
+from main import app
+from queries.saved_stocks import SavedStockQueries
 
-# client = TestClient(app)
-
-
-# class SavedStockQueriesMock:
-#     def test_get_all_saved_stocks(self):
-#         return {}
+client = TestClient(app)
 
 
-# def test_get_all_saved_stocks():
-
-#     # arrange
-#     app.dependency_overrides[SavedStockQueries] = SavedStockQueriesMock
-
-#     # act
-#     response = client.get("api/saved_stocks")
-
-#     # assert
-#     assert response.status_code == 200
+class SavedStockQueriesMock:
+    def test_get_all_saved_stocks(self):
+        return {}
 
 
-# def test_create_or_update_saved_stock():
+def test_get_all_saved_stocks():
 
-#     # arrange
-#     app.dependency_overrides[SavedStockQueries] = SavedStockQueriesMock
-#     # act
-#     client.get("api/saved_stocks")
-#     # assert
-#     # get a 200 for posting
-#     assert response.status_code == 200
+    # arrange
+    app.dependency_overrides[SavedStockQueries] = SavedStockQueriesMock
+
+    # act
+    response = client.get("api/saved_stocks")
+
+    # assert
+    assert response.status_code == 200
 
 
-# def test_delete_saved_stock():
+def test_create_or_update_saved_stock():
 
-#     # arrange
-#     app.dependency_overrides[SavedStockQueries] = SavedStockQueriesMock
-#     # act
-#     client.get("api/saved_stocks/{saved_stock_id}")
-#     # assert
-#     response.status_code == 200
+    # arrange
+    app.dependency_overrides[SavedStockQueries] = SavedStockQueriesMock
+    # act
+    client.get("api/saved_stocks")
+    # assert
+    # get a 200 for posting
+    assert response.status_code == 200
+
+
+def test_delete_saved_stock():
+
+    # arrange
+    app.dependency_overrides[SavedStockQueries] = SavedStockQueriesMock
+    # act
+    client.get("api/saved_stocks/{saved_stock_id}")
+    # assert
+    response.status_code == 200
