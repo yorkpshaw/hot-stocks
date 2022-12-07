94dfbf617f20c14ecf88ccf7df8922dc134336fd
karis courey <kariscourey@gmail.com>
2022-12-07 18:39:05 -0500

    update formatting

--- api/tests/test_company_routes.py
+++ api/tests/test_company_routes.py
@@ -4,20 +4,23 @@ from main import app
 # replace swagger in code
 client = TestClient(app)
 
+
 def test_get_company():
 
     # arrange
-    symbol = 'example'
+    symbol = "example"
 
     # act
-    response = client.get(f'/api/companies/{symbol}/')
+    response = client.get(f"/api/companies/{symbol}/")
 
     # assert
     # get a 200
     assert response.status_code == 200
 
     # response as expected
-    assert response.json() == {'company': {'description': None, 'name': None, 'symbol': None}}
+    assert response.json() == {
+        "company": {"description": None, "name": None, "symbol": None}
+    }
 
     # cleanup
     app.dependency_overrides = {}
--- api/tests/test_token_routes.py
+++ api/tests/test_token_routes.py
@@ -8,10 +8,11 @@ client = TestClient(app)
 
 def get_token_with_token():
     return {
-        'id': '500',
-        'username': 'example',
-        'email': 'example@example.com',
+        "id": "500",
+        "username": "example",
+        "email": "example@example.com",
     }
+
 
 def get_token_without_token():
     return None
@@ -20,17 +21,19 @@ def get_token_without_token():
 def test_get_token_with_token():
 
     # arrange
-    app.dependency_overrides[authenticator.try_get_current_account_data] = get_token_with_token
+    app.dependency_overrides[
+        authenticator.try_get_current_account_data
+    ] = get_token_with_token
 
     # act
-    response = client.get('/token', cookies={authenticator.cookie_name: 'example'})
+    response = client.get("/token", cookies={authenticator.cookie_name: "example"})
     data = response.json()
 
     # assert
     assert response.status_code == 200
-    assert data['access_token'] == 'example'
-    assert data['account']['id'] == '500'
-    assert data['token_type'] == 'Bearer'
+    assert data["access_token"] == "example"
+    assert data["account"]["id"] == "500"
+    assert data["token_type"] == "Bearer"
 
     # clean up
     app.dependency_overrides = {}
@@ -39,10 +42,12 @@ def test_get_token_with_token():
 def test_get_token_without_token():
 
     # arrange
-    app.dependency_overrides[authenticator.try_get_current_account_data] = get_token_without_token
+    app.dependency_overrides[
+        authenticator.try_get_current_account_data
+    ] = get_token_without_token
 
     # act
-    response = client.get('/token')
+    response = client.get("/token")
 
     # assert
     assert response.status_code == 200
@@ -55,32 +60,34 @@ def test_get_token_without_token():
 class AccountQueriesMock:
     def create(self, info, hashed_password):
         return {
-            'id': '500',
-            'username': 'user',
-            'email': 'user@example.com',
-            'hashed_password': '$2a$12$3NDZqR8ukkp9sHVK8H5.WOWeh70b7rpGwJgUpmRI3r0s8CXAtbk3G',
+            "id": "500",
+            "username": "user",
+            "email": "user@example.com",
+            "hashed_password": "$2a$12$3NDZqR8ukkp9sHVK8H5.WOWeh70b7rpGwJgUpmRI3r0s8CXAtbk3G",
         }
+
     def get(self, username):
         return {
-            'id': '500',
-            'username': 'user',
-            'email': 'user@example.com',
-            'hashed_password': '$2a$12$3NDZqR8ukkp9sHVK8H5.WOWeh70b7rpGwJgUpmRI3r0s8CXAtbk3G',
+            "id": "500",
+            "username": "user",
+            "email": "user@example.com",
+            "hashed_password": "$2a$12$3NDZqR8ukkp9sHVK8H5.WOWeh70b7rpGwJgUpmRI3r0s8CXAtbk3G",
         }
+
 
 def test_create_account():
 
     # arrange
     app.dependency_overrides[AccountQueries] = AccountQueriesMock
     account = {
-        'username': 'user',
-        'email': 'user@example.com',
-        'password': 'example',
+        "username": "user",
+        "email": "user@example.com",
+        "password": "example",
     }
 
     # act
     response = client.post(
-        '/api/accounts',
+        "/api/accounts",
         json=account,
     )
 
@@ -90,9 +97,9 @@ def test_create_account():
 
     # response as expected
     data = response.json()
-    assert data['access_token'] is not None
-    assert data['account']['id'] is not None
-    assert data['account']['username'] == 'user'
+    assert data["access_token"] is not None
+    assert data["account"]["id"] is not None
+    assert data["account"]["username"] == "user"
 
     # cleanup
     app.dependency_overrides = {}
