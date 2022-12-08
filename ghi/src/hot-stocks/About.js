6a437cc4e2afdbe85c99c14b66eaf117c322a576
karis courey <kariscourey@gmail.com>
2022-12-08 12:52:24 -0500

    clean up formatting in about

--- ghi/src/hot-stocks/About.js
+++ ghi/src/hot-stocks/About.js
@@ -1,39 +1,18 @@
-import * as React from 'react';
-import { Copyright } from '../common/Copyright';
-import { ErrorNotification } from '../common/ErrorNotification';
 import LocalFireDepartmentOutlinedIcon from '@mui/icons-material/LocalFireDepartmentOutlined';
 import { CssBaseline } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import { deepOrange } from '@mui/material/colors';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { eventTargetSelector as target, preventDefault } from '../common/utils';
-import { updateField } from '../rtk-files/accountSlice';
-import { useGetTokenQuery, useLogInMutation } from '../rtk-files/authApi';
-import { toggleSignUp } from '../rtk-files/signUpSlice';
-import { keys } from '@mui/system';
+import * as React from 'react';
+import { Copyright } from '../common/Copyright';
 
 
 const theme = createTheme();
 
 export function About() {
-
-    const { data: token, isLoading: tokenLoading } = useGetTokenQuery();
-    const dispatch = useDispatch();
-    const { username, password } = useSelector(state => state.account);
-    const [logIn, { error }] = useLogInMutation();
-    const field = useCallback(
-        e => dispatch(updateField({ field: e.target.name, value: e.target.value })),
-        [dispatch],
-    );
 
     const info = {
         'Explore': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.',
@@ -53,7 +32,8 @@ export function About() {
                         marginTop: 8,
                         display: 'flex',
                         flexDirection: 'column',
-                        alignItems: 'center'}}>
+                        alignItems: 'center'
+                    }}>
                     <Avatar sx={{ m: 1, bgcolor: deepOrange[500] }}>
                         <LocalFireDepartmentOutlinedIcon />
                     </Avatar>
@@ -63,14 +43,16 @@ export function About() {
                     sx={{
                         marginTop: 3,
                         display: 'flex',
-                        flexDirection: 'column'}}>
+                        flexDirection: 'column'
+                    }}>
                     {Object.keys(info).map((key, index) => (
                         <Box key={index}
                             sx={{
                                 marginTop: 3,
                                 display: 'flex',
                                 flexDirection: 'column',
-                                alignItems: 'left'}}>
+                                alignItems: 'left'
+                            }}>
                             <Typography component="h1" variant="h5">
                                 {key}
                             </Typography>
